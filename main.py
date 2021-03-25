0517acd9e9ac93755fcfd55c746f4a07cbb5da55
sashabusse <sasha.busse@mail.ru>
2021-03-26 00:16:37 +0300

    some intermediate commit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import sympy as sym
+from sympy import symbols, simplify, lambdify
+from sympy.vector import CoordSys3D, matrix_to_vector
 import numpy as np
-from scipy.integrate import odeint, solve_ivp
-import matplotlib.pyplot as plt
 import pandas as pd
 
 def pol2cart(r, tet, phi):
@@ -18,50 +19,57 @@ def cart2pol(vec):
     phi = np.arctan(y/x)
     return r, tet, phi
 
+def r_ort(tet, phi):
+    x = np.sin(tet)*np.cos(phi)
+    y = np.sin(tet)*np.sin(phi)
+    z = np.cos(tet)
+    return np.array([x, y, z])
 
-gamma = 5e8
-alpha = 0.018
-Hext = np.array(pol2cart(2000.0, np.deg2rad(80.0), 0.0))
-# Heff = np.array([0.0, 0.0, -2000.0])
-Hkeff = 6100.0
+def tet_ort(tet, phi):
+    x = np.cos(tet)*np.cos(phi)
+    y = np.cos(tet)*np.sin(phi)
+    z = -np.sin(tet)
+    return np.array([x, y, z])
 
+def phi_ort(tet, phi):
+    x = -np.sin(phi)
+    y = np.cos(phi)
+    z = 0
+    return np.array([x, y, z])
 
-def Heff(Hext, M):
-    H_abs, H_tet, H_phi = cart2pol(Hext)
-    M_abs, M_tet, M_phi = cart2pol(M)
-    r = H_abs * np.cos(H_tet - M_tet)
-    tet = H_abs * np.sin(H_tet - M_tet)
-    # r = -(H_abs*np.cos(H_tet - M_tet) - 0.5*H_abs*(np.cos(M_tet)**2))
-    # tet = -(H_abs*np.sin(H_tet - M_tet) + Hkeff*np.cos(M_tet)*np.sin(M_tet))
+S = CoordSys3D('S')
 
-    r_ort = np.array([np.sin(M_tet) * np.cos(M_phi), np.sin(M_tet) * np.sin(M_phi), np.cos(M_tet)])
-    tet_ort = np.array([np.cos(M_tet) * np.cos(M_phi), np.cos(M_tet) * np.sin(M_phi), -np.sin(M_tet)])
-    ret = r * r_ort + tet * tet_ort
-    return ret  # pol2cart(r, tet, 0)
+Ku = symbols("Ku")
+Mx, My, Mz = symbols('Mx My Mz')
+M = Mx * S.i + My * S.j + Mz * S.k
 
+sin_tet_sqr = (Mx ** 2 + My ** 2) / (Mx ** 2 + My ** 2 + Mz ** 2)
 
-def model(t, M):
-    Heff_loc = Heff(Hext, M)
-    Ms = np.linalg.norm(M)
-    first = np.cross(M, Heff_loc)
-    second = (alpha / Ms) * np.cross(M, np.cross(M, Heff_loc))
-    return -(gamma / (1 + alpha ** 2)) * (first + second)
+E = Ku * sin_tet_sqr
 
+Hx = simplify(-sym.diff(E, Mx))
+Hy = simplify(-sym.diff(E, My))
+Hz = simplify(-sym.diff(E, Mz))
 
-M0 = np.array([100.0, 0, 150.0])
-
-# t = np.linspace(0,500, 1000) #ps
-sol = solve_ivp(model, [0.0, 200.0e-12], M0, method='LSODA')
-
-df = pd.DataFrame()
-df['t'] = sol.t
-df['x'] = sol.y[0]
-df['y'] = sol.y[1]
-df['z'] = sol.y[2]
-# df = df.set_index('t')
+lambda_var_set = (Ku, Mx, My, Mz)
+Heff_x = lambdify(lambda_var_set, Hx, 'numpy')
+Heff_y = lambdify(lambda_var_set, Hy, 'numpy')
+Heff_z = lambdify(lambda_var_set, Hz, 'numpy')
 
 
-df.plot('t', figsize = (11, 6))
-#plt.plot(sol.t, sol.y[0]**2 + sol.y[1]**2 + sol.y[2]**2)
-plt.grid()
-plt.show()
+
+
+Kuv = 6e5
+tet = np.deg2rad(67)
+Mv = pol2cart(800e3, tet, 0.)
+Hmod = Kuv*np.sin(2*tet)/np.linalg.norm(Mv)
+
+
+print(Hmod * tet_ort(tet, 0))
+print([
+    Heff_x(Kuv, Mv[0], Mv[1], Mv[2]),
+    Heff_y(Kuv, Mv[0], Mv[1], Mv[2]),
+    Heff_z(Kuv, Mv[0], Mv[1], Mv[2]),
+])
+
+
